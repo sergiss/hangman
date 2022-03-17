ca71783517cf92f62393076e9fc7dd502da9cfcb
sergi <sergi.ss4@gmail.com>
2022-03-17 09:36:11 +0100

    Clean code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,4 @@
+var ok, nok;
 var word = "";
 var labels = [];
 
@@ -9,65 +10,61 @@ body.push(document.querySelector("#right-arm"));
 body.push(document.querySelector("#left-leg"));
 body.push(document.querySelector("#right-leg"));
 
-let failures = 0;
-
 const setWord = (value) => {
     labels = [];
-    let el = document.querySelector("#word-container");
+    const el = document.querySelector("#word-container");
     el.innerHTML = '';
-    for(let label, i = 0; i < value.length; ++i) {
-        label = document.createElement("label");
-        label.classList.add("label")
-        label.innerText = '';
-        el.appendChild(label);
-        labels[i] = label;
+    for(let i = 0; i < value.length; ++i) {
+        labels[i] = document.createElement("label");
+        labels[i].classList.add("label")
+        labels[i].innerText = '';
+        el.appendChild(labels[i]);
     }
     word = value;
-
     for(let i = 0; i < body.length; ++i) {
         body[i].style.visibility = "hidden";
     }
+    ok = nok = 0;
+    initKeyboard();
 }
 
 const initKeyboard = ()=> {
-    let el = document.querySelector("#keyboard");
+    const el = document.querySelector("#keyboard");
     el.innerHTML = "";
-
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
     for(let input, c, i = 0; i < alphabet.length; ++i) {
         c = alphabet.charAt(i);
-
         input = document.createElement("input");
         input.value = c;
         input.type = "button";
-        el.appendChild(input);
-
         input.addEventListener("click", (e) => {
-
-            if(failures < body.length)  {
-                const value = e.target.value;
-                e.target.disabled = true;
+         
+            if(ok < body.length && nok < body.length)  { // is clickable?
+                const value = e.target.value; // target value (character)
+                e.target.disabled = true;     // disable input button
     
                 const tmp = word.toLowerCase();
-                if(tmp.indexOf(value) > -1) {
+                if(tmp.indexOf(value) > -1) { // OK
                     let index = -1;
-                    while((index = tmp.indexOf(value, index + 1)) > -1) {
-                        labels[index].innerText = word.charAt(index);
+                    while((index = tmp.indexOf(value, index + 1)) > -1) { // iterate matches 
+                        labels[index].innerText = word.charAt(index);     // reveal
+                        ok++; // increase hits 
                     }
-                } else {
-                    body[failures].style.visibility = "visible";
-                    failures++;
-                    if(failures === body.length) {
-                         // TODO : 
+                    if(ok === body.length) { // check game over
+                        // TODO : win
+                   }
+                } else { // NOK
+                    body[nok].style.visibility = "visible"; // add body part
+                    nok++; // increase fails
+                    if(nok === body.length) { // check game over
+                         // TODO : lose
                     }
                 }
-                
             }
 
         });
-
+        el.appendChild(input);
     }
 }
 
 setWord("Alicia");
-initKeyboard();
